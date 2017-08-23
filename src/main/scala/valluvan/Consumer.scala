e7b1d5bad56a44bb0bc19d4409b11c147f96249e
Selva Valluvan <selva.valluvan@quickmobile.com>
2017-08-23 16:20:20 -0700

    Cleaning up

--- src/main/scala/valluvan/Consumer.scala
+++ src/main/scala/valluvan/Consumer.scala
@@ -12,8 +12,6 @@ import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.kinesis.KinesisUtils
 import org.apache.spark.streaming.{Milliseconds, StreamingContext}
 
-import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessor
-import com.amazonaws.services.kinesis.clientlibrary.lib.worker.ShutdownReason
 
 object Consumer {
     def main(args: Array[String]): Unit = {
@@ -46,7 +44,7 @@ object Consumer {
         val ssc = new StreamingContext(sparkConfig, batchInterval)
 
         val kinesisStreams = KinesisUtils.createStream(ssc, "quickstats-development-selva", kinesisStreamName, kinesisEndpointUrl, kinesisRegion,
-                InitialPositionInStream.LATEST, kinesisCheckpointInterval, StorageLevel.MEMORY_AND_DISK_2)
+            InitialPositionInStream.LATEST, kinesisCheckpointInterval, StorageLevel.MEMORY_AND_DISK_2)
 
         val streamingData = kinesisStreams.map(byteArray => {
             new String(byteArray)
@@ -57,7 +55,7 @@ object Consumer {
                 val file = new FileWriter("./log/sample.txt", true)
                 logger.info("Records in this batch: " + rdd.count())
                 logger.info("First Record: " + rdd.first())
-//                Thread.sleep(5000)
+                //                Thread.sleep(5000)
                 logger.info("Last Record: " + rdd.collect().last)
                 rdd.collect().foreach(data => {
                     file.write(data + "\n")
@@ -70,48 +68,16 @@ object Consumer {
 
 
         val mainThread = Thread.currentThread
-        Runtime.getRuntime.addShutdownHook(new Thread() {override def run = {
-            logger.info("HERE")
-            mainThread.join()
-        }})
+        Runtime.getRuntime.addShutdownHook(new Thread() {
+            override def run = {
+                logger.info("HERE")
+                mainThread.join()
+            }
+        })
 
         ssc.start()
         ssc.awaitTermination()
 
 
     }
-
-
-
-
-//    def main(args: Array[String]) {
-//        var keepRunning = true
-//
-//        val sparkConfig = new SparkConf().setAppName("quickstats-development-selva").setMaster("local[4]")
-//        val ssc = new StreamingContext(sparkConfig, Milliseconds(1000))
-//        val lines = ssc.socketTextStream("localhost", 9999)
-//        val words = lines.flatMap(_.split(" "))
-//        val pairs = words.map(word => (word, 1))
-//
-//        val wordCounts = pairs.reduceByKey(_ + _)
-//
-//        val mainThread = Thread.currentThread()
-//        Runtime.getRuntime.addShutdownHook(new Thread() {override def run = {
-//            println("inside addShutDownHook handler")
-//            keepRunning = false
-//            ssc.stop(stopSparkContext = true, stopGracefully = true)
-//            mainThread.join()
-//        }})
-//
-//        // Print the first ten elements of each RDD generated in this DStream to the console
-//        wordCounts.print()
-//        ssc.start()             // Start the computation
-//        ssc.awaitTermination()
-////        while (keepRunning) {
-////            println("in while loop")
-////            Thread.sleep(1000)
-////        }
-//        // Graceful shutdown code goes here that needs to be on the main thread
-//        println("Exiting main")
-//    }
 }
